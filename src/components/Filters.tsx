f0f18c2e61b5a46839faf199f4cb891ea39a1d43
JaeYeopHan <ljyhanll@gmail.com>
2019-12-14 15:37:05 +0900

    feat: Add Filter component style

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,34 +1,37 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import "./Filters.scss";
 
-import { Filter, todoActions } from "@/features/Todo";
+import classnames from "classnames";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { IRootState } from "@/features";
+import { Filter, TODO, todoActions, todoSelectors } from "@/features/Todo";
 
 export const Filters = () => {
   const dispatch = useDispatch();
+  const currentFilter = useSelector<IRootState, Filter>(state =>
+    todoSelectors.filter(state[TODO])
+  );
+  const filterButtonStyle = (val: Filter) =>
+    classnames("filter-button", {
+      "filter-button--active": val === currentFilter
+    });
 
   return (
-    <section>
-      <button
-        onClick={() =>
-          dispatch(todoActions.changeFilter({ filter: Filter.ALL }))
-        }
-      >
-        All
-      </button>
-      <button
-        onClick={() =>
-          dispatch(todoActions.changeFilter({ filter: Filter.ACTIVE }))
-        }
-      >
-        Active
-      </button>
-      <button
-        onClick={() =>
-          dispatch(todoActions.changeFilter({ filter: Filter.DONE }))
-        }
-      >
-        Done
-      </button>
+    <section className="filters">
+      {Object.values(Filter).map((value: Filter, index: number) => {
+        return (
+          <button
+            className={filterButtonStyle(value)}
+            key={`filter_button_${index}`}
+            onClick={() =>
+              dispatch(todoActions.changeFilter({ filter: value }))
+            }
+          >
+            {value}
+          </button>
+        );
+      })}
     </section>
   );
 };
