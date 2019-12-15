ee4b4366b348b8cabfc3d7afde595af7186e36e8
JaeYeopHan <ljyhanll@gmail.com>
2019-12-15 22:53:02 +0900

    feat: Add useRemount hooks

---
+++ src/hooks/useRemount.ts
@@ -0,0 +1,8 @@
+import { Dispatch, SetStateAction, useCallback, useState } from "react";
+
+export function useRemout(): [number, Dispatch<SetStateAction<number>>] {
+  const [key, setKey] = useState(0);
+  const remount = useCallback(() => setKey(key => key + 1), []);
+
+  return [key, remount];
+}
