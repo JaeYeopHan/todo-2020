1ef28a09c16311387c7679071f382954374e8ccf
JaeYeopHan <ljyhanll@gmail.com>
2019-12-16 13:47:08 +0900

    feat: Apply DrawerTransition

--- src/components/shared/Toast.tsx
+++ src/components/shared/Toast.tsx
@@ -3,6 +3,7 @@ import "./Toast.scss";
 import React, { ReactNode } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import { DrawerTransition } from '@/components/shared/transition/DrawerTransition'
 import { IRootState } from "@/features";
 import { IToast, TOAST, toastActions } from "@/features/Toast";
 
@@ -17,27 +18,25 @@ export const Toast = (props: IToastProps) => {
   const toast = useSelector<IRootState, IToast>(state => state[TOAST]);
   const isOpen = toast[props.id];
 
-  if (!isOpen) {
-    return <></>;
-  }
-
   return (
-    <div className="toast-dimmed">
-      <div className="toast-wrapper">
-        <h3 className="toast-title">Info</h3>
-        <div className="toast-contents">{props.children}</div>
-        <button
-          className="toast-button"
-          onClick={() => {
-            dispatch(toastActions.close(props.id));
-            if (props.onClose) {
-              props.onClose();
-            }
-          }}
-        >
-          OK
+    <div className="toast-dimmed" style={{ display: isOpen ? 'block' : 'none' }}>
+      <DrawerTransition in={isOpen}>
+        <div className="toast-wrapper">
+          <h3 className="toast-title">Info</h3>
+          <div className="toast-contents">{props.children}</div>
+          <button
+            className="toast-button"
+            onClick={() => {
+              dispatch(toastActions.close(props.id));
+              if (props.onClose) {
+                props.onClose();
+              }
+            }}
+          >
+            OK
         </button>
-      </div>
+        </div>
+      </DrawerTransition>
     </div>
   );
 };
