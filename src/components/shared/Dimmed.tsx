677b4739d72fa53705f9cd8a8700095b258b3d07
JaeYeopHan <ljyhanll@gmail.com>
2019-12-17 19:08:17 +0900

    feat: Add Dimmed component

---
+++ src/components/shared/Dimmed.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from 'react'
+
+interface IDimmedProps {
+  isShow: boolean
+  children: ReactNode
+}
+
+export const Dimmed = (props: IDimmedProps) => {
+  return (
+    <div className="dimmed" style={{ display: props.isShow ? 'block' : 'none' }}>
+      {props.children}
+    </div>
+  )
+}
