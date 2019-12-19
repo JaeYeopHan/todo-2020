35042bc1a169c29a3dc3bad5363f6b244648d19d
JaeYeopHan <ljyhanll@gmail.com>
2019-12-19 10:18:41 +0900

    feat: Apply useTouchStyle hooks to filter button

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -6,32 +6,41 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { IRootState } from "@/features";
 import { Filter, TODO, todoActions, todoSelectors } from "@/features/Todo";
+import { useTouchStyle } from "@/hooks/useTouchStyle";
 
 export const Filters = () => {
-  const dispatch = useDispatch();
-  const currentFilter = useSelector<IRootState, Filter>(state =>
-    todoSelectors.filter(state[TODO])
-  );
-  const filterButtonStyle = (val: Filter) =>
-    classnames("filters-button", {
-      "filters-button--active": val === currentFilter
-    });
-
   return (
     <section className="filters">
-      {Object.values(Filter).map((value: Filter, index: number) => {
-        return (
-          <button
-            className={filterButtonStyle(value)}
-            key={`filters_button_${index}`}
-            onClick={() =>
-              dispatch(todoActions.changeFilter({ filter: value }))
-            }
-          >
-            {value}
-          </button>
-        );
+      {Object.values(Filter).map((filter: Filter, index: number) => {
+        return <FilterButton key={`filter_button_${index}`} filter={filter} />;
       })}
     </section>
   );
 };
+
+interface IFilterButton {
+  filter: Filter
+}
+
+const FilterButton = (props: IFilterButton) => {
+  const { filter } = props
+  const dispatch = useDispatch();
+  const currentFilter = useSelector<IRootState, Filter>(state =>
+    todoSelectors.filter(state[TODO])
+  );
+  const filterButtonStyle = classnames("filters-button", {
+    "filters-button--active": filter === currentFilter
+  });
+  const touchStyleProps = useTouchStyle(filterButtonStyle, "filters-button--selected")
+
+  return (
+    <button
+      {...touchStyleProps}
+      onClick={() =>
+        dispatch(todoActions.changeFilter({ filter }))
+      }
+    >
+      {filter}
+    </button>
+  )
+}
